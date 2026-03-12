2784548fe8f866b9d7e3bc8689204807819550d7
NiKiT0S1 <bobilevnikita88@gmail.com>
2026-03-12 16:44:37 +0500

    Add StudentSpecification Class for multi-filtering. Update AdminController, StudentRepository, StudentService Classes

---
+++ backend/src/main/java/com/university/careerplatform/backend/specification/StudentSpecification.java
@@ -0,0 +1,48 @@
+package com.university.careerplatform.backend.specification;
+
+import com.university.careerplatform.backend.entity.Student;
+import org.springframework.data.jpa.domain.Specification;
+
+public class StudentSpecification {
+
+    public static Specification<Student> filterStudents(
+            String educationalProgram,
+            Integer course,
+            String practiceStatus,
+            Double minGpa
+    ) {
+        return (root, query, criteriaBuilder) -> {
+            var predicate = criteriaBuilder.conjunction();
+
+            if (educationalProgram != null && !educationalProgram.isBlank()) {
+                predicate = criteriaBuilder.and(
+                        predicate,
+                        criteriaBuilder.equal(root.get("educationalProgram"), educationalProgram)
+                );
+            }
+
+            if (course != null) {
+                predicate = criteriaBuilder.and(
+                        predicate,
+                        criteriaBuilder.equal(root.get("course"), course)
+                );
+            }
+
+            if (practiceStatus != null && !practiceStatus.isBlank()) {
+                predicate = criteriaBuilder.and(
+                        predicate,
+                        criteriaBuilder.equal(root.get("practiceStatus"), practiceStatus)
+                );
+            }
+
+            if (minGpa != null) {
+                predicate = criteriaBuilder.and(
+                        predicate,
+                        criteriaBuilder.greaterThanOrEqualTo(root.get("gpa"), minGpa)
+                );
+            }
+
+            return predicate;
+        };
+    }
+}
