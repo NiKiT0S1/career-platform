bbd5dd4da3133c8a7aed9cadb036a9d7ef606243
NiKiT0S1 <bobilevnikita88@gmail.com>
2026-03-12 13:20:38 +0500

    Add Student Controller Class and Company Update Request DTO

---
+++ backend/src/main/java/com/university/careerplatform/backend/dto/CompanyUpdateRequest.java
@@ -0,0 +1,11 @@
+package com.university.careerplatform.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CompanyUpdateRequest {
+
+    private String companyName;
+}
